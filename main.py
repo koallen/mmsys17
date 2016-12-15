8b61fc75c73771ba8eede5a86aee0a9b8702fc0d
koallen <weifengzi2009@gmail.com>
2016-12-15 19:11:20 +0800

    evenly split data for SVM

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import jieba
 from sklearn import svm
 import numpy as np
@@ -10,32 +11,57 @@ def gen_data(sentence, sentence_dict, dictionary):
     """
     split = 0.7 # split between training and test set
 
-    dict_list = list(dictionary.keys())
-    data = list()
-    label = list()
+    pos_data = list()
+    neg_data = list()
+    neu_data = list()
     for sentence in sentences:
-        word_l = list()
+        word_l = jieba.lcut(sentence)
         features = list()
-        for word in jieba.cut(sentence):
-            word_l.append(word)
-        for word in dict_list:
+        for word in word_list:
             if word in word_l:
                 features.append(1)
             else:
                 features.append(0)
         if sentence_dict[sentence] == "p":
-            label.append(1)
+            pos_data.append(features)
         elif sentence_dict[sentence] == "n":
-            label.append(2)
+            neg_data.append(features)
         else:
-            label.append(0)
-        data.append(features)
+            neu_data.append(features)
+    pos_label = np.full((len(pos_data),), 2)
+    neg_label = np.full((len(neg_data),), 1)
+    neu_label = np.full((len(neu_data),), 0)
 
-    split_index = int(len(data) * split)
-    training_set = np.asarray(data[:split_index])
-    training_label = np.asarray(label[:split_index])
-    test_set = np.asarray(data[split_index:])
-    test_label = np.asarray(label[split_index:])
+    pos_split_index = int(len(pos_data) * split)
+    neg_split_index = int(len(neg_data) * split)
+    neu_split_index = int(len(neu_data) * split)
+
+    training_pos_data = np.asarray(pos_data[:pos_split_index])
+    training_neg_data = np.asarray(neg_data[:neg_split_index])
+    training_neu_data = np.asarray(neu_data[:neu_split_index])
+    training_set = np.concatenate((training_pos_data, training_neg_data, training_neu_data))
+
+    training_pos_label = pos_label[:pos_split_index]
+    training_neg_label = neg_label[:neg_split_index]
+    training_neu_label = neu_label[:neu_split_index]
+    training_label = np.concatenate((training_pos_label, training_neg_label, training_neu_label))
+
+    test_pos_data = np.asarray(pos_data[pos_split_index:])
+    test_neg_data = np.asarray(neg_data[neg_split_index:])
+    test_neu_data = np.asarray(neu_data[neu_split_index:])
+    test_set = np.concatenate((test_pos_data, test_neg_data, test_neu_data))
+
+    test_pos_label = pos_label[pos_split_index:]
+    test_neg_label = neg_label[neg_split_index:]
+    test_neu_label = neu_label[neu_split_index:]
+    test_label = np.concatenate((test_pos_label, test_neg_label, test_neu_label))
+
+    print(training_pos_data.shape)
+    print(training_neg_data.shape)
+    print(training_neu_data.shape)
+    print(test_pos_data.shape)
+    print(test_neg_data.shape)
+    print(test_neu_data.shape)
 
     return training_set, training_label, test_set, test_label
 
@@ -75,6 +101,17 @@ if __name__ == "__main__":
     sentences = corpus.sentence_list
     sentence_dict = corpus.sentence_dict
     print("Dictionaries and corpus loaded successfully")
+
+    with open("dictionaries/new_corpus.json", "r") as corpus_json:
+        corpus = json.load(corpus_json)
+        word_list = list()
+        for sentence in corpus:
+            for word in sentence["contain_words"]:
+                actual_word = word["word"]
+                if actual_word not in word_list:
+                    word_list.append(actual_word)
+
+    print(len(word_list))
 
     # SVM
     svm_classify(sentences, sentence_dict, dictionary)
