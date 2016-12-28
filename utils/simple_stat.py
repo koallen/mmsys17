1fb6c598bc651be7a0bf71c5ee3cc4d9584aaf3d
koallen <weifengzi2009@gmail.com>
2016-12-28 18:21:20 +0800

    add script to generate some stats

---
+++ utils/simple_stat.py
@@ -0,0 +1,35 @@
+import json
+import matplotlib.pyplot as plt
+
+with open("../dictionaries/new_corpus.json", "r") as corpus_json:
+    corpus = json.load(corpus_json)
+
+length_list = list()
+for sentence in corpus:
+    length = len(sentence["content"])
+    length_list.append(length)
+
+plt.hist(length_list, bins = 50, color="b")
+plt.xlabel("Sentence length")
+plt.ylabel("Frequency")
+plt.show()
+
+pos_count = 0
+neu_count = 0
+neg_count = 0
+
+for sentence in corpus:
+    if sentence["sentiment"] == "p":
+        pos_count += 1
+    elif sentence["sentiment"] == "n":
+        neg_count += 1
+    elif sentence["sentiment"] == "z":
+        neu_count += 1
+
+x = [pos_count, neu_count, neg_count]
+y = [0, 1, 2]
+y_label = ["Positive", "Neutral", "Negative"]
+
+plt.bar(y, x, color="b", align="center", width=0.5)
+plt.xticks(y, y_label)
+plt.show()
